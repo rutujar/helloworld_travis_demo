2fece8d86cd246073c7c05f88d6efbb00a94a05e
rutujar <rutujarajesh23@gmail.com>
2018-07-26 09:53:55 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+       int num = 10, count = 1, total = 0;
+       while(count <= num)
+       {
+           total = total + count;
+           count++;
+       }
+       System.out.println("Sum of first 10 natural numbers is: "+total);
+    }
+}
