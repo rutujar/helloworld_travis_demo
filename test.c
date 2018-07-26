42cd66e36c78c63ddaeb33dc07e7d30fd6c58762
rutujar <rutujarajesh23@gmail.com>
2018-07-26 09:29:23 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int i, j, rows;
+    int i, j, rows=5;
     printf("Enter number of rows: ");
-    scanf("%d",&rows);
     for(i=1; i<=rows; ++i)
     {
         for(j=1; j<=i; ++j)
